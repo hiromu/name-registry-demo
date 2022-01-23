6e3af18fecc382662d92f43f70e10aba66f42717
Hiromu Yakura <hiromu1996@gmail.com>
2022-01-24 01:50:20 +0900

    Updates

---
+++ precache-manifest.de71d6292ffbc83f7c662c29e76bfdbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e2f3d65b017c7b4b9b4d0cfc41337e",
+    "url": "/name-registry-demo/index.html"
+  },
+  {
+    "revision": "8b14d7f52c3f50f34326",
+    "url": "/name-registry-demo/static/css/main.78f2af24.chunk.css"
+  },
+  {
+    "revision": "cd77fb81b3b47b6c92e0",
+    "url": "/name-registry-demo/static/js/2.b827df1e.chunk.js"
+  },
+  {
+    "revision": "8b14d7f52c3f50f34326",
+    "url": "/name-registry-demo/static/js/main.90a52460.chunk.js"
+  },
+  {
+    "revision": "87a1e74bf0250392f1c8",
+    "url": "/name-registry-demo/static/js/runtime-main.4f663a17.js"
+  }
+]);
